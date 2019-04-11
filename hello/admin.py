3def2d934433883f387ffcec1a37e7b7416bbff3
Travis <tg@realemail.com>
2019-04-11 11:31:18 -0500

    added problem model to admin page

--- hello/admin.py
+++ hello/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from .models import AreaCoordinate
+from .models import Problem
 
 # Register your models here.
 admin.site.register(AreaCoordinate)
+admin.site.register(Problem)
