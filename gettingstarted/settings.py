040944773e848138c652714aeaa94c37f1ea818a
Travis <tg@realemail.com>
2019-04-11 11:27:12 -0500

    model migrations

--- gettingstarted/settings.py
+++ gettingstarted/settings.py
@@ -78,7 +78,15 @@ WSGI_APPLICATION = "gettingstarted.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE" : "django.db.backends.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.path.join(BASE_DIR, "db.postgresql"),
+        "USER": "",
+        "PASSWORD": "",
+        "HOST": "localhost",
+        "PORT": "5432"
+    },
+    "original": {
+        "ENGINE": "django.db.backends.sqlite3",
         "NAME": os.path.join(BASE_DIR, "db.sqlite3")
     }
 }
