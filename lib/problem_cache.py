f6f7b8e20580bccdec38338fc6df3567bca622b8
Travis <tg@realemail.com>
2019-04-16 11:04:48 -0500

    Fixed bug in problem_cache.py

--- lib/problem_cache.py
+++ lib/problem_cache.py
@@ -11,8 +11,8 @@ def cache_problems_at_coordinates(lat, lon, area_id):
     for problem in boulder_payload:
         problem_models.append(processor.create_problem(
             mp_id=problem.mp_id, boulder=True, location=problem.location,
-            lat=problem.lat, lon=problem.lon, name=problem.name, area_id=area_id),
-            mp_url=problem.url, grade=problem.grade)
+            lat=problem.lat, lon=problem.lon, name=problem.name, area_id=area_id,
+            mp_url=problem.url, grade=problem.grade))
 
     route_payload = mp_api.get_routes_at_coordinates(lat, lon)
     for problem in route_payload:
