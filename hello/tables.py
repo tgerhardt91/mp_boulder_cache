2f84bade5d33828b2900e2ee549ceaf9eef065d6
Travis <tg@realemail.com>
2019-04-22 13:17:10 -0500

    Column bug fix

--- hello/tables.py
+++ hello/tables.py
@@ -4,12 +4,12 @@ import django_tables2 as tables
 
 
 class ProblemTable(tables.Table):
-    class Meta:
-        model = Problem
-        template_name = 'django_tables2/bootstrap.html'
-
     name = tables.Column()
     grade = tables.Column()
     map = tables.TemplateColumn('<a href="{{record.gmap_url}}">map link</a>')
     location = tables.Column()
     mp_url = tables.Column()
+
+    class Meta:
+        model = Problem
+        template_name = 'django_tables2/bootstrap.html'
